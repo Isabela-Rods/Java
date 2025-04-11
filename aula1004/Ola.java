e6d6be3b21c5338f6be7f80e49b4e54bb1a02877
Isabela-Rods <isabela.rodrigues172008@gmail.com>
2025-04-11 12:00:53 -0300

    Commit incial da calculadora

--- aula1004/Ola.java
+++ aula1004/Ola.java
@@ -1,5 +1,6 @@
-public class Ola{
-    public static void main(String[] args){
+public class Ola {
+    public static void main (String[] args){
         System.out.println("Boa tarde à todos!");
     }
 }
+
